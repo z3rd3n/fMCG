2cfa96136990bd1244204071deddd8c2bbbbad35
23rd3n <serden.eranil@tum.de>
2024-05-26 13:45:43 +0000

    training part

--- autoencoder/utils.py
+++ autoencoder/utils.py
@@ -7,4 +7,9 @@ def calculate_kurtosis(tensor):
     kurtosis_values = stats.kurtosis(tensor_np, axis=1, nan_policy='omit')
     # Replace NaNs with 0
     kurtosis_values = np.nan_to_num(kurtosis_values, nan=0.0)
-    return torch.tensor(kurtosis_values, dtype=torch.float32)
+    return torch.tensor(kurtosis_values, dtype=torch.float32, requires_grad=True)
+
+def calculate_psnr(original, reconstructed, max_val=1.0):
+    mse = F.mse_loss(original, reconstructed)
+    psnr = 20 * torch.log10(max_val / torch.sqrt(mse))
+    return psnr
